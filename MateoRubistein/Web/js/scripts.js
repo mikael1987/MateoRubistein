fd835ff36191252099b2294b34d4be7cd4d03373
Miguel Girón <miguel.giron5@gmail.com>
2020-08-23 17:34:13 +0200

    Pestaña noticias
    Signed-off-by: Miguel Girón <miguel.giron5@gmail.com>

--- MateoRubistein/Web/js/scripts.js
+++ MateoRubistein/Web/js/scripts.js
@@ -4,85 +4,74 @@
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
     */
 (function ($) {
-    "use strict"; // Start of use strict
+	"use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: (target.offset().top - 72)
-                }, 1000, "easeInOutExpo");
-                return false;
-            }
-        }
-    });
+	// Smooth scrolling using jQuery easing
+	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: (target.offset().top - 72)
+				}, 1000, "easeInOutExpo");
+				return false;
+			}
+		}
+	});
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function () {
-        $('.navbar-collapse').collapse('hide');
-    });
+	// Closes responsive menu when a scroll trigger link is clicked
+	$('.js-scroll-trigger').click(function () {
+		$('.navbar-collapse').collapse('hide');
+	});
 
-    // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-        target: '#mainNav',
-        offset: 75
-    });
+	// Activate scrollspy to add active class to navbar items on scroll
+	$('body').scrollspy({
+		target: '#mainNav',
+		offset: 75
+	});
 
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-scrolled");
-        } else {
-            $("#mainNav").removeClass("navbar-scrolled");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
+	// Collapse Navbar
+	var navbarCollapse = function () {
+		if ($("#mainNav").offset().top > 100) {
+			$("#mainNav").addClass("navbar-scrolled");
+		} else {
+			$("#mainNav").removeClass("navbar-scrolled");
+		}
+	};
+	// Collapse now if page is not at top
+	navbarCollapse();
+	// Collapse the navbar when page is scrolled
+	$(window).scroll(navbarCollapse);
 
-    //Magnific popup calls
-    $('#portfolio').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1]
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
-    });
+	//Magnific popup calls
+	$('.portfolio-box').magnificPopup({
+		type: 'image'
+	});
     /*
 Carousel
 */
-    $('#carousel-example').on('slide.bs.carousel', function (e) {
-        /*
-            CC 2.0 License Iatek LLC 2018 - Attribution required
-        */
-        var $e = $(e.relatedTarget);
-        var idx = $e.index();
-        var itemsPerSlide = 5;
-        var totalItems = $('.carousel-item').length;
+	//$('#recipeCarousel').carousel({
+	//	interval: 500000
+	//})
 
-        if (idx >= totalItems - (itemsPerSlide - 1)) {
-            var it = itemsPerSlide - (totalItems - idx);
-            for (var i = 0; i < it; i++) {
-                // append slides to end
-                if (e.direction == "left") {
-                    $('.carousel-item').eq(i).appendTo('.carousel-inner');
-                }
-                else {
-                    $('.carousel-item').eq(0).appendTo('.carousel-inner');
-                }
-            }
-        }
-    });
+	//$('.carousel .carousel-item').each(function () {
+	//	var next = $(this).next();
+	//	if (!next.length) {
+	//		next = $(this).siblings(':first');
+	//	}
+	//	next.children(':first-child').clone().appendTo($(this));
 
-})(jQuery); // End of use strict
+	//	for (var i = 0; i < 2; i++) {
+	//		next = next.next();
+	//		if (!next.length) {
+	//			next = $(this).siblings(':first');
+	//		}
+
+	//		next.children(':first-child').clone().appendTo($(this));
+	//	}
+	//});
+
+
+
+}) (jQuery); // End of use strict
